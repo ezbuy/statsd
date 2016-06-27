936d370ae2965a34e8a252338ae5622b23bd9215
hongjianzhu <zhu.life@gmail.com>
2016-06-27 14:00:55 +0800

    add unit test

--- statsd_test.go
+++ statsd_test.go
@@ -1,19 +1,43 @@
 package statsd
 
-import "testing"
+import (
+	"testing"
+	"time"
+)
 
 func initConfig() {
 	config := &Config{
-		Host:   "192.168.199.61",
-		Port:   8125,
-		Enable: true,
+		Project: "ezt",
+		Host:    "192.168.199.61",
+		Port:    8125,
+		Enable:  true,
 	}
 
 	Setup(config)
 }
 
-func TestGauge(t *testing.T) {
+func Test_Gauge(t *testing.T) {
 	initConfig()
 
-	Gauge("myproj.login.count", 10)
+	v := 0
+
+	ticker := time.NewTicker(100 * time.Millisecond)
+	for {
+		<-ticker.C
+
+		v++
+		Gauge("statsd.gauge", v)
+	}
+}
+
+func Test_Incr(t *testing.T) {
+	initConfig()
+
+	ticker := time.NewTicker(100 * time.Millisecond)
+
+	for {
+		<-ticker.C
+
+		Incr("statsd.incr")
+	}
 }
